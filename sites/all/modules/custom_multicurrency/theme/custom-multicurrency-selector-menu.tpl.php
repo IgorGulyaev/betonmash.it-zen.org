d9f0f1081556f5d53dfdb933c712994c801ea3e8
Igor Gulyaev <Igor Gulyaev>
2017-01-03 03:18:05 +0200

    Minor fixes

--- sites/all/modules/custom_multicurrency/theme/custom-multicurrency-selector-menu.tpl.php
+++ sites/all/modules/custom_multicurrency/theme/custom-multicurrency-selector-menu.tpl.php
@@ -7,11 +7,25 @@
  * $enabled_currencies
  * $user_currency
  */
+
+$cur_label = t("Валюта:");
 ?>
-<ul class="currency_select_menu">
-<?php foreach($enabled_currencies as $currency) : ?>
-  <li class="<?php print $currency['code'] . (($currency['code'] == $user_currency) ? ' active' : NULL); ?>">
-    <a href="<?php print url('commerce_currency_select/' . $currency['code'], array('query' => drupal_get_destination())); ?>"><?php print $currency['code']; ?></a>
-  </li>
-<?php endforeach;?>
-</ul>
+
+<div class="currency_select_menu dropdown">
+  <span class="label"><?php echo $cur_label; ?></span>
+  <?php foreach($enabled_currencies as $currency) : ?>
+      <?php if(($currency['code'] == $user_currency)): ?>
+        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><?php print $currency['code']; ?>
+        <span class="caret"></span></button>
+      <?php endif; ?>
+  <?php endforeach;?>
+  <ul class="dropdown-menu">
+    <?php foreach($enabled_currencies as $currency) : ?>
+      <?php if(($currency['code'] != $user_currency)): ?>
+      <li class="<?php print $currency['code']; ?>">
+        <a href="<?php print url('commerce_currency_select/' . $currency['code'], array('query' => drupal_get_destination())); ?>"><?php print $currency['code']; ?></a>
+      </li>
+      <?php endif; ?>
+    <?php endforeach;?>
+  </ul>
+</div>
